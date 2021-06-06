da7495c31faf6aa6f74c28c64e8f1a85971a18af
robsonvidigal <robsonvidigal@gmail.com>
2021-06-05 22:26:03 -0300

    Add files via upload

--- codigomain/codemain.py
+++ codigomain/codemain.py
@@ -1,4 +1,4 @@
-#  Jogo jokenpó - v1.0.0.1
+#  Jogo jokenpo - v1.0.0.2
 
 from random import randint
 from time import sleep
@@ -11,7 +11,7 @@ vit_jogador = 0
 empate = 0
 vit_cpu = 0
 encerrar = ' '
-desing = 15
+design = 15
 
 while encerrar != 'N':
     itens = ('Pedra', 'Papel', 'Tesoura')
@@ -30,60 +30,60 @@ Computador: {vit_cpu}
     jogador = int(input('Escolha sua jogada: '))
     if jogador < 3:
         jogadas += 1
-        print('-=' * desing)
+        print('-=' * design)
         print('Computador jogou: {}' .format(itens[cpu]))
         print('Jogador escolheu: {}' .format(itens[jogador]))
     if cpu == 0:  # Computador joga pedra.
         if jogador == 0:
             print('Jogada Emptada!')
-            print('-=' * desing)
+            print('-=' * design)
             empate += 1
         elif jogador == 1:
             print('Jogador é o vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_jogador += 1
         elif jogador == 2:
             print('Computador é o vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_cpu += 1
         else:
             print('Jogada inválida.')
-            print('-=' * desing)
+            print('-=' * design)
     elif cpu == 1:  # Computador joga papel.
         if jogador == 0:
             print('Computador é o vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_cpu += 1
         elif jogador == 1:
             print('Jogada Empatada!')
-            print('-=' * desing)
+            print('-=' * design)
             empate += 1
         elif jogador == 2:
             print('Jogador é o vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_jogador += 1
         else:
             print('Jogada inválida.')
-            print('-=' * desing)
+            print('-=' * design)
     elif cpu == 2:  # Computador joga tesoura.
         if jogador == 0:
             print('Jogador é o Vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_jogador += 1
         elif jogador == 1:
             print('Computador é o vencedor!')
-            print('-=' * desing)
+            print('-=' * design)
             vit_cpu += 1
         elif jogador == 2:
             print('Jogada Empatada!')
-            print('-=' * desing)
+            print('-=' * design)
             empate += 1
         else:
             print('Jogada inválida.')
-            print('-=' * desing)
+            print('-=' * design)
     encerrar = str(input('\nDesejar continuar jogando [S/N]? ')).upper().strip()[0]
 print('')
-print('-=' * desing)
+print('-=' * design)
 print('FIM DO JOGO!')
 print(f'''JOGADAS {jogadas}
 << Placar Final >>
@@ -91,4 +91,4 @@ Jogador: {vit_jogador}
 Empates: {empate}
 Computador: {vit_cpu}
 ''')
-print('-=' * desing)
+print('-=' * design)
